7b53013c344b76d6ececeb32ddfcfc49bbf34d6e
ThoaiDP <dinhthoai241297@gmail.com>
2021-07-17 01:28:04 +0700

    Update appServer file

--- source/appServer.js
+++ source/appServer.js
@@ -2,15 +2,13 @@ var express = require('express')
 var path = require('path')
 var compression = require('compression')
 
-/*eslint-disable no-console */
-
 const port = {PORT}
 const app = express()
 
 app.use(compression())
-//app.use(express.static('dist'));
-
 app.use('/static', express.static(path.join(__dirname, './static')))
+app.use('/', express.static(path.join(__dirname, './')))
+
 app.get('*', function (req, res) {
     console.log('request: ===> ' + req._parsedUrl.pathname)
     res.sendFile(path.join(__dirname, 'index.html'))
@@ -20,6 +18,6 @@ app.listen(port, function (err) {
     if (err) {
         console.log(err)
     } else {
-        console.log('server start success')
+        console.log('server start success: ' + port)
     }
 })
